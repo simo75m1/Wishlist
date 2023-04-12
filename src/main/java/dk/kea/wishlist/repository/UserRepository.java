bbd8540c402ab06c36ab4f4846539ae4d058e721
simo75m1 <simo75m1@stud.kea.dk>
2023-04-12 14:04:25 +0200

    login page created. Can check user credentials

--- src/main/java/dk/kea/wishlist/repository/UserRepository.java
+++ src/main/java/dk/kea/wishlist/repository/UserRepository.java
@@ -2,12 +2,14 @@ package dk.kea.wishlist.repository;
 
 import dk.kea.wishlist.utility.ConnectionManager;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@Repository
 public class UserRepository
 {
     @Value("${spring.datasource.url}")
