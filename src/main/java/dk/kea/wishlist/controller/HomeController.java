85a02025de55597468523900ba9bc98dea7195c1
simo75m1 <simo75m1@stud.kea.dk>
2023-04-12 11:53:15 +0200

    Created login mapping to test branch

--- src/main/java/dk/kea/wishlist/controller/HomeController.java
+++ src/main/java/dk/kea/wishlist/controller/HomeController.java
@@ -1,8 +1,14 @@
 package dk.kea.wishlist.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController
 {
+    @GetMapping("/login")
+    public String showLogin()
+    {
+        return "login";
+    }
 }
